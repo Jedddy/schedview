4e3b385b929f4da4b6d7f9c9857570db8a4ad63c
Jedddy <jedbalita25@gmail.com>
2024-02-18 23:09:13 +0800

    feat: add time picker

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:sched_view/models/schedule.dart' as models;
+import 'package:sched_view/screens/time_picker.dart';
+
 // import 'package:sched_view/models/schedule.dart' as models;
 
 // class ScheduleData extends StatelessWidget {
@@ -9,42 +12,73 @@ import 'package:flutter/material.dart';
 // }
 
 class Schedule extends StatefulWidget {
+  final int groupId;
   final String groupName;
 
-  const Schedule({super.key, required this.groupName});
+  const Schedule({super.key, required this.groupId, required this.groupName});
 
   @override
   State<Schedule> createState() => _Schedule();
 }
 
 class _Schedule extends State<Schedule> {
+  late Map<String, List<models.Schedule>> _schedules;
+
+  @override
+  void initState() {
+    Future.microtask(() async {
+      final schedules = await models.getSchedules(widget.groupId);
+
+      setState(() {
+        _schedules = schedules;
+      });
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 7,
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text(widget.groupName),
-              bottom: const TabBar(tabs: [
-                Tab(text: "M"),
-                Tab(text: "T"),
-                Tab(text: "W"),
-                Tab(text: "TH"),
-                Tab(text: "F"),
-                Tab(text: "S"),
-                Tab(text: "SU"),
-              ]),
-            ),
-            body: const TabBarView(
-              children: [
-                Icon(Icons.ice_skating),
-                Icon(Icons.ice_skating),
-                Icon(Icons.ice_skating),
-                Icon(Icons.ice_skating),
-                Icon(Icons.ice_skating),
-                Icon(Icons.ice_skating),
-                Icon(Icons.ice_skating),
-              ],
-            )));
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.groupName),
+          bottom: const TabBar(tabs: [
+            Tab(text: "M"),
+            Tab(text: "T"),
+            Tab(text: "W"),
+            Tab(text: "TH"),
+            Tab(text: "F"),
+            Tab(text: "S"),
+            Tab(text: "SU"),
+          ]),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.ice_skating),
+            Icon(Icons.ice_skating),
+            Icon(Icons.ice_skating),
+            Icon(Icons.ice_skating),
+            Icon(Icons.ice_skating),
+            Icon(Icons.ice_skating),
+            Icon(Icons.ice_skating),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => TimePickerInput(
+                    groupId: widget.groupId,
+                  ),
+                ));
+          },
+          tooltip: "Add Schedule",
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/time_picker.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+import 'package:sched_view/models/schedule.dart';
+
+class TimePickerInput extends StatefulWidget {
+  final int groupId;
+
+  const TimePickerInput({super.key, required this.groupId});
+
+  @override
+  State<TimePickerInput> createState() => _TimePickerInput();
+}
+
+class _TimePickerInput extends State<TimePickerInput> {
+  TimeOfDay _selectedTimeStart = const TimeOfDay(hour: 0, minute: 0);
+  TimeOfDay _selectedTimeEnd = const TimeOfDay(hour: 0, minute: 0);
+  String _selectedDay = "M";
+  final TextEditingController _controller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  _selectTime(BuildContext context, String time) async {
+    TimeOfDay t;
+
+    if (time == "start") {
+      t = _selectedTimeStart;
+    } else {
+      t = _selectedTimeEnd;
+    }
+
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: t,
+    );
+
+    if (picked != null && picked != t) {
+      setState(() {
+        if (time == "start") {
+          _selectedTimeStart = picked;
+        } else {
+          _selectedTimeEnd = picked;
+        }
+      });
+    }
+  }
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Schedule"),
+      ),
+      body: Column(
+        children: <Widget>[
+          DropdownButton(
+            value: _selectedDay,
+            items: const [
+              DropdownMenuItem(
+                value: "M",
+                child: Text("Monday"),
+              ),
+              DropdownMenuItem(
+                value: "T",
+                child: Text("Tuesday"),
+              ),
+              DropdownMenuItem(
+                value: "W",
+                child: Text("Wednesday"),
+              ),
+              DropdownMenuItem(
+                value: "TH",
+                child: Text("Thursday"),
+              ),
+              DropdownMenuItem(
+                value: "F",
+                child: Text("Friday"),
+              ),
+              DropdownMenuItem(
+                value: "S",
+                child: Text("Saturday"),
+              ),
+              DropdownMenuItem(
+                value: "SU",
+                child: Text("Sunday"),
+              )
+            ],
+            onChanged: (val) {
+              setState(() {
+                _selectedDay = val!;
+              });
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 12, right: 12),
+            child: Form(
+              key: _formKey,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Schedule Label"),
+                ),
+                validator: (val) {
+                  if (val == null || val.isEmpty) {
+                    return "Label is empty.";
+                  }
+                  return null;
+                },
+                controller: _controller,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("Time Start: ${_selectedTimeStart.format(context)}"),
+                  Text("Time End: ${_selectedTimeEnd.format(context)}"),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () async => await _selectTime(context, "start"),
+                    icon: const Icon(Icons.timer_rounded),
+                    label: const Text("Start"),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () async => await _selectTime(context, "end"),
+                    icon: const Icon(Icons.timer_rounded),
+                    label: const Text("End"),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        insertSchedule(
+                          widget.groupId,
+                          _controller.text,
+                          _selectedDay,
+                          _formatTimeOfDay(_selectedTimeStart),
+                          _formatTimeOfDay(_selectedTimeEnd),
+                        );
+
+                        Navigator.pop(context);
+                      }
+                    },
+                    icon: const Icon(Icons.check),
+                    label: const Text("Submit"),
+                  ),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+String _formatTimeOfDay(TimeOfDay time) {
+  final hours = time.hour.toString().padLeft(2, '0');
+  final minutes = time.minute.toString().padLeft(2, '0');
+  return '$hours:$minutes';
+}

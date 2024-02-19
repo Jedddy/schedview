fa4e773ac037a032e383b6926691602e11257d53
Jedddy <jedbalita25@gmail.com>
2024-02-19 23:05:27 +0800

    refactor: move delete dialog to utils

---
+++ lib/utils.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Future<dynamic> deleteDialog(BuildContext context, String label, Function() cb) {
+  return showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text("Delete $label?"),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text("Cancel"),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            cb();
+            Navigator.pop(context);
+          },
+          child: const Text("OK"),
+        ),
+      ],
+    ),
+  );
+}
